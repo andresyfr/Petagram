2cd26a2ded374cb33212cad9ae8fb55b051c3d07
andresyfr <andresyfr@gmail.com>
2017-02-06 00:24:42 -0500

    floating botton implementado mensajes por snackbar implementado

--- app/src/main/java/com/andresyfr/petagram/MainActivity.java
+++ app/src/main/java/com/andresyfr/petagram/MainActivity.java
@@ -1,7 +1,12 @@
 package com.andresyfr.petagram;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFabBton();
     }
+
+    public void agregarFabBton(){
+        final FloatingActionButton fabton=(FloatingActionButton)findViewById(R.id.floatingActionButton);
+        fabton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_LONG).show();
+                Snackbar.make(v,getResources().getString(R.string.mensaje),Snackbar.LENGTH_SHORT)
+                        .setAction(getResources().getString(R.string.textAction), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("SNACKBAR", "Click en SnackBar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                        .show();
+            }
+        });
+    }
+
 }
